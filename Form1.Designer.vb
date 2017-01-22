9d15d534a0a9a62dbb3ef4550051439ab7d84701
UWLuCheng <lucheng9712@gmail.com>
2017-01-21 21:21:59 -0500

    Flappy Birds
    Another version of Flappy Birds

---
+++ Form1.Designer.vb
@@ -0,0 +1,362 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pbxBird = New System.Windows.Forms.PictureBox()
+        Me.tmrGravity = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrXSpeed = New System.Windows.Forms.Timer(Me.components)
+        Me.pbxPU4 = New System.Windows.Forms.PictureBox()
+        Me.pbxPD4 = New System.Windows.Forms.PictureBox()
+        Me.pbxPU5 = New System.Windows.Forms.PictureBox()
+        Me.pbxPD5 = New System.Windows.Forms.PictureBox()
+        Me.pbxPD6 = New System.Windows.Forms.PictureBox()
+        Me.pbxPU6 = New System.Windows.Forms.PictureBox()
+        Me.pbxPD7 = New System.Windows.Forms.PictureBox()
+        Me.pbxPU7 = New System.Windows.Forms.PictureBox()
+        Me.pbxPD3 = New System.Windows.Forms.PictureBox()
+        Me.pbxPU3 = New System.Windows.Forms.PictureBox()
+        Me.pbxPD1 = New System.Windows.Forms.PictureBox()
+        Me.pbxPU1 = New System.Windows.Forms.PictureBox()
+        Me.pbxPD2 = New System.Windows.Forms.PictureBox()
+        Me.pbxPU2 = New System.Windows.Forms.PictureBox()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.tmrLevel = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HighScoreToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EndToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.pbxBird, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPU4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPD4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPU5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPD5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPD6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPU6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPD7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPU7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPD3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPU3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPD1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPU1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPD2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPU2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pbxBird
+        '
+        Me.pbxBird.Image = CType(resources.GetObject("pbxBird.Image"), System.Drawing.Image)
+        Me.pbxBird.Location = New System.Drawing.Point(20, 230)
+        Me.pbxBird.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxBird.Name = "pbxBird"
+        Me.pbxBird.Size = New System.Drawing.Size(33, 25)
+        Me.pbxBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbxBird.TabIndex = 5
+        Me.pbxBird.TabStop = False
+        '
+        'tmrGravity
+        '
+        Me.tmrGravity.Interval = 40
+        '
+        'tmrXSpeed
+        '
+        Me.tmrXSpeed.Interval = 40
+        '
+        'pbxPU4
+        '
+        Me.pbxPU4.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPU4.Location = New System.Drawing.Point(1527, 31)
+        Me.pbxPU4.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPU4.Name = "pbxPU4"
+        Me.pbxPU4.Size = New System.Drawing.Size(60, 113)
+        Me.pbxPU4.TabIndex = 7
+        Me.pbxPU4.TabStop = False
+        '
+        'pbxPD4
+        '
+        Me.pbxPD4.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPD4.Location = New System.Drawing.Point(1527, 15)
+        Me.pbxPD4.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPD4.Name = "pbxPD4"
+        Me.pbxPD4.Size = New System.Drawing.Size(60, 514)
+        Me.pbxPD4.TabIndex = 8
+        Me.pbxPD4.TabStop = False
+        '
+        'pbxPU5
+        '
+        Me.pbxPU5.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPU5.Location = New System.Drawing.Point(1753, 31)
+        Me.pbxPU5.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPU5.Name = "pbxPU5"
+        Me.pbxPU5.Size = New System.Drawing.Size(60, 190)
+        Me.pbxPU5.TabIndex = 9
+        Me.pbxPU5.TabStop = False
+        '
+        'pbxPD5
+        '
+        Me.pbxPD5.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPD5.Location = New System.Drawing.Point(1753, 15)
+        Me.pbxPD5.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPD5.Name = "pbxPD5"
+        Me.pbxPD5.Size = New System.Drawing.Size(60, 514)
+        Me.pbxPD5.TabIndex = 10
+        Me.pbxPD5.TabStop = False
+        '
+        'pbxPD6
+        '
+        Me.pbxPD6.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPD6.Location = New System.Drawing.Point(1980, 15)
+        Me.pbxPD6.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPD6.Name = "pbxPD6"
+        Me.pbxPD6.Size = New System.Drawing.Size(60, 514)
+        Me.pbxPD6.TabIndex = 12
+        Me.pbxPD6.TabStop = False
+        '
+        'pbxPU6
+        '
+        Me.pbxPU6.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPU6.Location = New System.Drawing.Point(1980, 31)
+        Me.pbxPU6.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPU6.Name = "pbxPU6"
+        Me.pbxPU6.Size = New System.Drawing.Size(60, 263)
+        Me.pbxPU6.TabIndex = 11
+        Me.pbxPU6.TabStop = False
+        '
+        'pbxPD7
+        '
+        Me.pbxPD7.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPD7.Location = New System.Drawing.Point(2207, 15)
+        Me.pbxPD7.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPD7.Name = "pbxPD7"
+        Me.pbxPD7.Size = New System.Drawing.Size(60, 514)
+        Me.pbxPD7.TabIndex = 14
+        Me.pbxPD7.TabStop = False
+        '
+        'pbxPU7
+        '
+        Me.pbxPU7.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPU7.Location = New System.Drawing.Point(2207, 31)
+        Me.pbxPU7.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPU7.Name = "pbxPU7"
+        Me.pbxPU7.Size = New System.Drawing.Size(60, 113)
+        Me.pbxPU7.TabIndex = 13
+        Me.pbxPU7.TabStop = False
+        '
+        'pbxPD3
+        '
+        Me.pbxPD3.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPD3.Location = New System.Drawing.Point(1300, 15)
+        Me.pbxPD3.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPD3.Name = "pbxPD3"
+        Me.pbxPD3.Size = New System.Drawing.Size(60, 514)
+        Me.pbxPD3.TabIndex = 16
+        Me.pbxPD3.TabStop = False
+        '
+        'pbxPU3
+        '
+        Me.pbxPU3.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPU3.Location = New System.Drawing.Point(1300, 31)
+        Me.pbxPU3.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPU3.Name = "pbxPU3"
+        Me.pbxPU3.Size = New System.Drawing.Size(60, 44)
+        Me.pbxPU3.TabIndex = 15
+        Me.pbxPU3.TabStop = False
+        '
+        'pbxPD1
+        '
+        Me.pbxPD1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPD1.Location = New System.Drawing.Point(847, 15)
+        Me.pbxPD1.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPD1.Name = "pbxPD1"
+        Me.pbxPD1.Size = New System.Drawing.Size(60, 514)
+        Me.pbxPD1.TabIndex = 20
+        Me.pbxPD1.TabStop = False
+        '
+        'pbxPU1
+        '
+        Me.pbxPU1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPU1.Location = New System.Drawing.Point(847, 31)
+        Me.pbxPU1.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPU1.Name = "pbxPU1"
+        Me.pbxPU1.Size = New System.Drawing.Size(60, 241)
+        Me.pbxPU1.TabIndex = 19
+        Me.pbxPU1.TabStop = False
+        '
+        'pbxPD2
+        '
+        Me.pbxPD2.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPD2.Location = New System.Drawing.Point(1073, 15)
+        Me.pbxPD2.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPD2.Name = "pbxPD2"
+        Me.pbxPD2.Size = New System.Drawing.Size(60, 514)
+        Me.pbxPD2.TabIndex = 18
+        Me.pbxPD2.TabStop = False
+        '
+        'pbxPU2
+        '
+        Me.pbxPU2.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.pbxPU2.Location = New System.Drawing.Point(1073, 31)
+        Me.pbxPU2.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbxPU2.Name = "pbxPU2"
+        Me.pbxPU2.Size = New System.Drawing.Size(60, 172)
+        Me.pbxPU2.TabIndex = 17
+        Me.pbxPU2.TabStop = False
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(53, 47)
+        Me.lblScore.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(0, 52)
+        Me.lblScore.TabIndex = 21
+        '
+        'tmrLevel
+        '
+        Me.tmrLevel.Interval = 30000
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(8, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(349, 28)
+        Me.MenuStrip1.TabIndex = 22
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.HighScoreToolStripMenuItem, Me.EndToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(58, 24)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        Me.MenuToolStripMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'StartToolStripMenuItem
+        '
+        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
+        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(152, 24)
+        Me.StartToolStripMenuItem.Text = "Start"
+        '
+        'HighScoreToolStripMenuItem
+        '
+        Me.HighScoreToolStripMenuItem.Name = "HighScoreToolStripMenuItem"
+        Me.HighScoreToolStripMenuItem.Size = New System.Drawing.Size(152, 24)
+        Me.HighScoreToolStripMenuItem.Text = "High Score"
+        '
+        'EndToolStripMenuItem
+        '
+        Me.EndToolStripMenuItem.Name = "EndToolStripMenuItem"
+        Me.EndToolStripMenuItem.Size = New System.Drawing.Size(152, 24)
+        Me.EndToolStripMenuItem.Text = "End"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(156, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 17)
+        Me.Label2.TabIndex = 23
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(349, 502)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.pbxPD1)
+        Me.Controls.Add(Me.pbxPU1)
+        Me.Controls.Add(Me.pbxPD2)
+        Me.Controls.Add(Me.pbxPU2)
+        Me.Controls.Add(Me.pbxPD3)
+        Me.Controls.Add(Me.pbxPU3)
+        Me.Controls.Add(Me.pbxPD7)
+        Me.Controls.Add(Me.pbxPU7)
+        Me.Controls.Add(Me.pbxPD6)
+        Me.Controls.Add(Me.pbxPU6)
+        Me.Controls.Add(Me.pbxPD5)
+        Me.Controls.Add(Me.pbxPU5)
+        Me.Controls.Add(Me.pbxPD4)
+        Me.Controls.Add(Me.pbxPU4)
+        Me.Controls.Add(Me.pbxBird)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbxBird, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPU4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPD4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPU5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPD5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPD6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPU6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPD7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPU7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPD3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPU3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPD1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPU1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPD2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPU2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pbxBird As System.Windows.Forms.PictureBox
+    Friend WithEvents tmrGravity As System.Windows.Forms.Timer
+    Friend WithEvents tmrXSpeed As System.Windows.Forms.Timer
+    Friend WithEvents pbxPU4 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPD4 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPU5 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPD5 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPD6 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPU6 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPD7 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPU7 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPD3 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPU3 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPD1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPU1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPD2 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxPU2 As System.Windows.Forms.PictureBox
+    Friend WithEvents lblScore As System.Windows.Forms.Label
+    Friend WithEvents tmrLevel As System.Windows.Forms.Timer
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents StartToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EndToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HighScoreToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,240 @@
+﻿Public Class Form1
+
+    ' Lucy Cheng
+    ' Flappy Birds (Own)
+    ' May 2015
+
+    Dim intYSpeed As Integer = 0
+    Dim intGravity As Integer = 2
+    Dim shtXSpeed As Short = 5
+    Dim shtScore As Short = 0
+    Dim shtGap As Short = 115
+    Dim shtHighScore As Short
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Space Then
+            intYSpeed = -15
+        End If
+    End Sub
+
+    'Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    '    Me.tmrGravity.Enabled = True
+    '    Me.tmrXSpeed.Enabled = True
+    '    Me.tmrLevel.Enabled = True
+    '    RandomizePipeSize()
+    'End Sub
+
+    Private Sub tmrGravity_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrGravity.Tick
+        intYSpeed += intGravity
+        Me.pbxBird.Top += intYSpeed
+        Me.lblScore.Text = shtScore.ToString
+    End Sub
+
+    Private Sub tmrLevel_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrLevel.Tick
+        shtXSpeed += 1
+    End Sub
+
+    Private Sub tmrXSpeed_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrXSpeed.Tick
+        MovePipeLeft()
+        LoopPipe()
+        Collision()
+    End Sub
+
+    Private Sub LoopPipe()
+        If pbxPU1.Left < -45 Then
+            pbxPU1.Left += 1195
+            pbxPD1.Left += 1195
+            Me.pbxPU1.Height = (250 - 50 + 1) * Rnd() + 50
+            Me.pbxPD1.Top = Me.pbxPU1.Height + shtGap
+            shtScore += 1
+        End If
+        If pbxPU2.Left < -45 Then
+            pbxPU2.Left += 1195
+            pbxPD2.Left += 1195
+            Me.pbxPU2.Height = (250 - 50 + 1) * Rnd() + 50
+            Me.pbxPD2.Top = Me.pbxPU2.Height + shtGap
+            shtScore += 1
+        End If
+        If pbxPU3.Left < -45 Then
+            pbxPU3.Left += 1195
+            pbxPD3.Left += 1195
+            Me.pbxPU3.Height = (250 - 50 + 1) * Rnd() + 50
+            Me.pbxPD3.Top = Me.pbxPU3.Height + shtGap
+            shtScore += 1
+        End If
+        If pbxPU4.Left < -45 Then
+            pbxPU4.Left += 1195
+            pbxPD4.Left += 1195
+            Me.pbxPU4.Height = (250 - 50 + 1) * Rnd() + 50
+            Me.pbxPD4.Top = Me.pbxPU4.Height + shtGap
+            shtScore += 1
+        End If
+        If pbxPU5.Left < -45 Then
+            pbxPU5.Left += 1195
+            pbxPD5.Left += 1195
+            Me.pbxPU5.Height = (250 - 50 + 1) * Rnd() + 50
+            Me.pbxPD5.Top = Me.pbxPU5.Height + shtGap
+            shtScore += 1
+        End If
+        If pbxPU6.Left < -45 Then
+            pbxPU6.Left += 1195
+            pbxPD6.Left += 1195
+            Me.pbxPU6.Height = (250 - 50 + 1) * Rnd() + 50
+            Me.pbxPD6.Top = Me.pbxPU6.Height + shtGap
+            shtScore += 1
+        End If
+        If pbxPU7.Left < -45 Then
+            pbxPU7.Left += 1195
+            pbxPD7.Left += 1195
+            Me.pbxPU7.Height = (250 - 50 + 1) * Rnd() + 50
+            Me.pbxPD7.Top = Me.pbxPU7.Height + shtGap
+            shtScore += 1
+        End If
+    End Sub
+
+    Private Sub RandomizePipeSize()
+        Randomize()
+        Me.pbxPU1.Height = (250 - 50 + 1) * Rnd() + 50
+        Me.pbxPU2.Height = (250 - 50 + 1) * Rnd() + 50
+        Me.pbxPU3.Height = (250 - 50 + 1) * Rnd() + 50
+        Me.pbxPU4.Height = (250 - 50 + 1) * Rnd() + 50
+        Me.pbxPU5.Height = (250 - 50 + 1) * Rnd() + 50
+        Me.pbxPU6.Height = (250 - 50 + 1) * Rnd() + 50
+        Me.pbxPU7.Height = (250 - 50 + 1) * Rnd() + 50
+        Me.pbxPD1.Top = Me.pbxPU1.Height + shtGap
+        Me.pbxPD2.Top = Me.pbxPU2.Height + shtGap
+        Me.pbxPD3.Top = Me.pbxPU3.Height + shtGap
+        Me.pbxPD4.Top = Me.pbxPU4.Height + shtGap
+        Me.pbxPD5.Top = Me.pbxPU5.Height + shtGap
+        Me.pbxPD6.Top = Me.pbxPU6.Height + shtGap
+        Me.pbxPD7.Top = Me.pbxPU7.Height + shtGap
+    End Sub
+
+    Private Sub MovePipeLeft()
+        Me.pbxPU1.Left -= shtXSpeed
+        Me.pbxPU2.Left -= shtXSpeed
+        Me.pbxPU3.Left -= shtXSpeed
+        Me.pbxPU4.Left -= shtXSpeed
+        Me.pbxPU5.Left -= shtXSpeed
+        Me.pbxPU6.Left -= shtXSpeed
+        Me.pbxPU7.Left -= shtXSpeed
+        Me.pbxPD1.Left -= shtXSpeed
+        Me.pbxPD2.Left -= shtXSpeed
+        Me.pbxPD3.Left -= shtXSpeed
+        Me.pbxPD4.Left -= shtXSpeed
+        Me.pbxPD5.Left -= shtXSpeed
+        Me.pbxPD6.Left -= shtXSpeed
+        Me.pbxPD7.Left -= shtXSpeed
+    End Sub
+
+    Private Sub Collision()
+        If pbxBird.Bounds.IntersectsWith(pbxPU1.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPU2.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPU3.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPU4.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPU5.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPU6.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPU7.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPD1.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPD2.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPD3.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPD4.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPD5.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPD6.Bounds) _
+            Or pbxBird.Bounds.IntersectsWith(pbxPD7.Bounds) _
+            Then
+            Me.tmrGravity.Enabled = False
+            Me.tmrXSpeed.Enabled = False
+            Me.tmrLevel.Enabled = True
+            MessageBox.Show("You Died")
+            If shtScore > shtHighScore Then
+                shtHighScore = shtScore
+                MessageBox.Show("You beat your high score! Your new High Score is: " + shtHighScore.ToString)
+                'ElseIf shtScore = shtHighScore Then
+                '    MessageBox.Show("Need just one more pipe! Your High Score is: " + shtHighScore.ToString)
+                'ElseIf shtScore < shtHighScore Then
+                '    MessageBox.Show("Needs More work! Your High Score is: " + shtHighScore.ToString)
+            End If
+        End If
+        If pbxBird.Top > 450 Then
+            Me.pbxBird.Top = 450
+            Me.tmrGravity.Enabled = False
+            Me.tmrXSpeed.Enabled = False
+            Me.tmrLevel.Enabled = True
+            MessageBox.Show("You Died")
+            If shtScore > shtHighScore Then
+                shtHighScore = shtScore
+                MessageBox.Show("You beat your high score! Your new High Score is: " + shtHighScore.ToString)
+                'ElseIf shtScore = shtHighScore Then
+                '    MessageBox.Show("Need just one more pipe! Your High Score is: " + shtHighScore.ToString)
+                'ElseIf shtScore < shtHighScore Then
+                '    MessageBox.Show("Needs More work! Your High Score is: " + shtHighScore.ToString)
+            End If
+        End If
+        If pbxBird.Top < 0 Then
+            Me.pbxBird.Top = 0
+            Me.tmrGravity.Enabled = False
+            Me.tmrXSpeed.Enabled = False
+            Me.tmrLevel.Enabled = True
+            MessageBox.Show("You Died")
+            If shtScore > shtHighScore Then
+                shtHighScore = shtScore
+                MessageBox.Show("You beat your high score! Your new High Score is: " + shtHighScore.ToString)
+                'ElseIf shtScore = shtHighScore Then
+                '    MessageBox.Show("Need just one more pipe! Your High Score is: " + shtHighScore.ToString)
+                'ElseIf shtScore < shtHighScore Then
+                '    MessageBox.Show("Needs More work! Your High Score is: " + shtHighScore.ToString)
+            End If
+        End If
+    End Sub
+
+    Private Sub StartToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles StartToolStripMenuItem.Click
+        Me.pbxBird.Left = 20
+        Me.pbxBird.Top = 230
+        Me.pbxPU1.Left = 635
+        Me.pbxPU1.Top = 25
+        Me.pbxPU2.Left = 805
+        Me.pbxPU2.Top = 25
+        Me.pbxPU3.Left = 975
+        Me.pbxPU3.Top = 25
+        Me.pbxPU4.Left = 1145
+        Me.pbxPU4.Top = 25
+        Me.pbxPU5.Left = 1315
+        Me.pbxPU5.Top = 25
+        Me.pbxPU6.Left = 1485
+        Me.pbxPU6.Top = 25
+        Me.pbxPU7.Left = 1655
+        Me.pbxPU7.Top = 25
+        Me.pbxPD1.Left = 635
+        Me.pbxPD1.Top = 12
+        Me.pbxPD2.Left = 805
+        Me.pbxPD2.Top = 12
+        Me.pbxPD3.Left = 975
+        Me.pbxPD3.Top = 12
+        Me.pbxPD4.Left = 1145
+        Me.pbxPD4.Top = 12
+        Me.pbxPD5.Left = 1315
+        Me.pbxPD5.Top = 12
+        Me.pbxPD6.Left = 1485
+        Me.pbxPD6.Top = 12
+        Me.pbxPD7.Left = 1655
+        Me.pbxPD7.Top = 12
+        intYSpeed = 0
+        intGravity = 2
+        shtGap = 115
+        shtXSpeed = 5
+        shtScore = 0
+        Me.tmrGravity.Enabled = True
+        Me.tmrXSpeed.Enabled = True
+        Me.tmrLevel.Enabled = True
+        RandomizePipeSize()
+    End Sub
+
+    Private Sub EndToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EndToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub HighScoreToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HighScoreToolStripMenuItem.Click
+        MessageBox.Show("Your High Score is: " + shtHighScore.ToString)
+    End Sub
+End Class
